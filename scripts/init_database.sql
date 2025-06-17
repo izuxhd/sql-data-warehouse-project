6378939fe217ab41d663fb0f88601ca0fc250028
Nikhil Sharma <63258571+izuxhd@users.noreply.github.com>
2025-06-17 13:15:26 +0530

    Create init_database.sql

---
+++ scripts/init_database.sql
@@ -0,0 +1,17 @@
+
+
+use master;
+ IF  EXISTS(select  1  from  databases where name='Datawarehouse')
+   BEGIN
+   ALTER  database DataWarehouse  set  SINGLE_USER  WITH  ROLLBACK IMMEDIATE;
+   DROP DATABASE Datawarehouse;
+
+   END
+   
+create  database DataWarehouse;
+USE DataWarehouse;
+create schema  bronze;
+GO
+create schema  silver;
+GO
+create schema  gold;
